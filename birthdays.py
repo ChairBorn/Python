05bb0c1a99fd3e39a8be8066b1065e1c99a0ec84
ChairBorn <mysakyuriy@gmail.com>
2024-08-16 16:21:43 -0700

    birthday function updated
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- birthdays.py
+++ birthdays.py
@@ -1,1 +1,16 @@
-birthdays  = { 'Alice': 'Apr 1', 'Bob': 'Dec 1'}
+birthdays  = { 'Alice': 'Apr 1', 'Bob': 'Dec 1', 'Carol': 'Mar 4'}
+
+while True:
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == '':
+        break
+    
+    if name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+        print('I do not have a birthday information for ' + name)
+        print('What is their birthday?')
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated')
