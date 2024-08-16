bd9af147a95d4410bbd975d33ce22b1979b61da4
ChairBorn <mysakyuriy@gmail.com>
2024-08-16 12:18:54 -0700

    calculate next step in the game
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- gameOfLife.py
+++ gameOfLife.py
@@ -21,3 +21,19 @@ while True: # Main Program Loop
         for x in range(WIDTH):
             print(currentCells[x][y], end='') # Print the # of space
         print() # Print a new line at the end of the row
+        
+    # Calculate the next step cells based on current step cells
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            # Get neighboring coordinates
+            # `% WIDTH` ensures leftCoord is always between 0 and WIDTH - 1
+            leftCoord = (x - 1) % WIDTH
+            rightCoord = (x + 1) % WIDTH
+            aboveCoord = (y - 1) % HEIGHT
+            belowCoord = (y + 1) % HEIGHT
+            
+            # Count Number of living neighbors
+            numNeighbors = 0
+            if currentCells[leftCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top Left Neighbor is alive
+                
