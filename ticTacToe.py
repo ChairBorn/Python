0fb373de6ac6808eea35c8b8cea8f964fece40f5
ChairBorn <mysakyuriy@gmail.com>
2024-08-20 16:42:53 -0700

    user input function
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,6 +1,7 @@
 theBoard = {'Top-L': ' ', 'Top-M': ' ', 'Top-R': ' ', # The Data Structure that models the TicTacToe board
             'Mid-L': ' ', 'Mid-M': ' ', 'Mid-R': ' ',
             'Low-L': ' ', 'Low-M': ' ', 'Low-R': ' '}
+# All the keys need to be passed for the function to work
 
 def printBoard(board):
     print(board['Top-L'] + '|' + board['Top-M'] + '|' + board['Top-R'])
@@ -9,4 +10,16 @@ def printBoard(board):
     print('-+-+-')
     print(board['Low-L'] + '|' + board['Low-M'] + '|' + board['Low-R'])
     print('-+-+-')
+
+# Let's take User's input
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
 printBoard(theBoard)
