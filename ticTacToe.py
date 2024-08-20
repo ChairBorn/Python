32cfd4aace7fa909a2836c2fbf2a2fabf1cfa1ff
ChairBorn <mysakyuriy@gmail.com>
2024-08-20 16:19:41 -0700

    start tic tac toe game
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

---
+++ ticTacToe.py
@@ -0,0 +1,7 @@
+theBoard = {'Top-L': ' ', 'Top-M': ' ', 'Top-R': ' ',
+            'Mid-L': ' ', 'Mid-M': ' ', 'Mid-R': ' ',
+            'Low-L': ' ', 'Low-M': ' ', 'Low-R': ' '}
+
+def printBoard(board):
+    print(board['Top-L'] + '|' + board['Top-M'] + '|' + board['Top-R'])
+    print('+-+-+-+')
