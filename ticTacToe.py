33b23ad47f88e5e6bf7a4908f4ca16ac4877cd8b
ChairBorn <mysakyuriy@gmail.com>
2024-08-20 16:21:51 -0700

    finish print board
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- ticTacToe.py
+++ ticTacToe.py
@@ -5,3 +5,7 @@ theBoard = {'Top-L': ' ', 'Top-M': ' ', 'Top-R': ' ',
 def printBoard(board):
     print(board['Top-L'] + '|' + board['Top-M'] + '|' + board['Top-R'])
     print('+-+-+-+')
+    print(board['Mid-L'] + '|' + board['Mid-M'] + '|' + board['Mid-R'])
+    print('+-+-+-+')
+    print(board['Low-L'] + '|' + board['Low-M'] + '|' + board['Low-R'])
+    print('+-+-+-+')
