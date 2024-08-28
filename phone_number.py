de9998f33b84b88bcbfb341967486471a3a3978a
ChairBorn <mysakyuriy@gmail.com>
2024-08-28 15:36:18 -0700

    comment
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- phone_number.py
+++ phone_number.py
@@ -1,6 +1,6 @@
 import re
 
-phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)') # Parentheses are used to group the area code and main number, r stands for raw string
 mo = phoneNumRegex.search('My number is 322-444-4567.')
 area_code, main_number = mo.groups() # mo.groups() returns a tuple
 print('Phone number found: ' + 'Area code: ' + area_code + ', Main number: ' + main_number)
