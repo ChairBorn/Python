4ef488ad403313320441eae639c72929717dd640
ChairBorn <mysakyuriy@gmail.com>
2024-08-28 14:50:48 -0700

    regex adjust
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- phone_number.py
+++ phone_number.py
@@ -1,5 +1,5 @@
 import re
 
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
 mo = phoneNumRegex.search('My number is 322-444-4567.')
-print('Phone number found: ' + mo.group())
+print('Phone number found:', mo.group())
