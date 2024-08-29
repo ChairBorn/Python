deb5dcde4fd6dbbc9d740e6875c7baf1d91c477a
ChairBorn <mysakyuriy@gmail.com>
2024-08-29 10:07:10 -0700

    add caret and dollar sign functions
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- phone_number.py
+++ phone_number.py
@@ -4,3 +4,7 @@ phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)') # Parentheses are used
 mo = phoneNumRegex.search('My number is 322-444-4567.')
 area_code, main_number = mo.groups() # mo.groups() returns a tuple
 print('Phone number found: ' + 'Area code: ' + area_code + ', Main number: ' + main_number)
+
+begins_num = re.compile(r'^\d+')
+begins_num.search('12 goats')
+print(begins_num)
