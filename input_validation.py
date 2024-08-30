936eb1cb5bfc772404c3ad003f63c321d09dd481
ChairBorn <mysakyuriy@gmail.com>
2024-08-29 18:56:44 -0700

    inputs defined
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- input_validation.py
+++ input_validation.py
@@ -1,1 +1,6 @@
 import pyinputplus as pyip
+
+responseNum = pyip.inputNum() # Accepts only numbers as input
+responseStr = pyip.inputStr() # Accepts only strings as input
+responseYesNo = pyip.inputYesNo() # Accepts only 'yes' or 'no' as input
+responseMenu = pyip.inputMenu(['a', 'b', 'c']) # Accepts only 'a', 'b', or 'c' as input
