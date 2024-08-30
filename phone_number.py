0946ea57a476c18ade1f2451a189de2573fdae28
ChairBorn <mysakyuriy@gmail.com>
2024-08-29 17:56:13 -0700

    comment
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- phone_number.py
+++ phone_number.py
@@ -3,7 +3,7 @@ import re
 phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)') # Parentheses are used to group the area code and main number, r stands for raw string
 mo = phoneNumRegex.search('My number is 322-444-4567.')
 area_code, main_number = mo.groups() # mo.groups() returns a tuple
-print('Phone number found: ' + 'Area code: ' + area_code + ', Main number: ' + main_number)
+print('Phone number found: ' + 'Area code: ' + area_code + ', Main number: ' + main_number) # 322, 444-4567
 
 begins_num = re.compile(r'^\d+') # ^ means the string must begin with a number
 print(begins_num.search('1234 is a number').group()) # 1234, instead of console output, we can use group() to get the matched string
