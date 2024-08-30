c296aa48d89958f741d14c6358f6e0939dee8cf8
ChairBorn <mysakyuriy@gmail.com>
2024-08-29 19:09:50 -0700

    add funcs
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- input_validation.py
+++ input_validation.py
@@ -4,3 +4,5 @@ responseNum = pyip.inputNum() # Accepts only numbers as input
 responseStr = pyip.inputStr() # Accepts only strings as input
 responseYesNo = pyip.inputYesNo() # Accepts only 'yes' or 'no' as input
 responseMenu = pyip.inputMenu(['a', 'b', 'c']) # Accepts only 'a', 'b', or 'c' as input
+responseChoice = pyip.inputEmail() # Accepts only email addresses as input
+responsePath = pyip.inputFilepath() # Accepts only file paths as input
