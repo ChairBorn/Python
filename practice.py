6671acd404a95eea87958032b8a206c361f4fdda
ChairBorn <mysakyuriy@gmail.com>
2024-09-02 15:16:29 -0700

    add smart guy game
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

---
+++ practice.py
@@ -0,0 +1,7 @@
+# Define the list
+my_list = ['a', 'b', 'c', 'd']
+
+# Loop through the list with enumerate
+for index, value in enumerate(my_list):
+    print(f"Index: {index}, Value: {value}")
+    
