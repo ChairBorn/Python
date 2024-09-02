3bb2002a61bb86236ab4e168efd93369e2749e24
ChairBorn <mysakyuriy@gmail.com>
2024-09-02 14:32:07 -0700

    adding input validation functions
    Signed-off-by: ChairBorn <mysakyuriy@gmail.com>

--- input_validation.py
+++ input_validation.py
@@ -6,3 +6,5 @@ responseYesNo = pyip.inputYesNo() # Accepts only 'yes' or 'no' as input
 responseMenu = pyip.inputMenu(['a', 'b', 'c']) # Accepts only 'a', 'b', or 'c' as input
 responseChoice = pyip.inputEmail() # Accepts only email addresses as input
 responsePath = pyip.inputFilepath() # Accepts only file paths as input
+
+response = pyip.inputNum('Enter a number: ', min=1, lessThan=100) # Prompts user to enter a number
